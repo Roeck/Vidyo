f48a79c4850fca7ddf36ff840f952f92083d276c
R B <rons9n@gmail.com>
2021-01-22 18:15:08 -0500

    initial FollowersColumn and Header components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,2 @@
-import './App.css';
 
-const App = () => {
- return (
-    <div className="App">
-    
-    </div>
-  );
-}
 
-export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,12 @@
+import FollowersColumn from '../components/FollowersColumn';
+
 const Home = () => {
     return (
         <div>
             <h1>Home</h1>
+            <FollowersColumn />
+            <div className="feed"></div>
+            <div className="suggested-box"></div>
         </div>
     )
 }
