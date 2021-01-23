af4307b9bb8ffadcc888d3e61fa10570e43e7bcf
R B <rons9n@gmail.com>
2021-01-23 18:00:46 -0500

    defined astra props

--- functions/addData.js
+++ functions/addData.js
@@ -1,7 +1,5 @@
 const { createClient } = require("@astrajs/collections") 
 
-const { createClient } = require("@astrajs/collections")
-
 const collection = "tktkposts"
 
 exports.handler = async function (event, context, callback) {
@@ -9,7 +7,7 @@ exports.handler = async function (event, context, callback) {
     astraDatabaseId: process.env.ASTRA_DB_ID,
     astraDatabaseRegion: process.env.ASTRA_DB_REGION,
     username: process.env.ASTRA_DB_USERNAME,
-    password: process.env.ASTRA_DB_PASSWORD,
+    password: process.env.,
   })
 
   const posts = astraClient
